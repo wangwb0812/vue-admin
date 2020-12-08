dee099fb2148f0171d595a1dc3a8763a89fc77c5
wwb <1007575954@qq.com>
2020-12-08 11:20:12 +0800

    wwb

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,10 @@
   </div>
 </template>
 
+<style lang="scss" scoped>
+	#app {
+		height: 100%;
+	}
+</style>
+
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="login">
-		<el-form ref="form" :model="form" label-width="80px">
-			<el-form-item label="用户名">
-				<el-input v-model="form.username"></el-input>
+		<div class="login-form-wrapper">
+			<h1 class="login-title">用户登录</h1>
+			<el-form class="login-form" ref="form" :model="form" :rules="rules">
+				<el-form-item prop="username">
+					<el-input v-model="form.username" placeholder="用户名"></el-input>
 				</el-form-item>
-			<el-form-item label="密码">
-				<el-input type="password" v-model="form.password"></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button :loading="loading" type="primary" @click="handleLogin">登录</el-button>
-			</el-form-item>
-		</el-form>
+				<el-form-item prop="password">
+					<el-input type="password" v-model="form.password" placeholder="密码"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button class="login-btn" :loading="loading" type="primary" @click="handleLogin">登录</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
 	</div>
 </template>
 
@@ -18,24 +21,47 @@
 import { mapActions } from 'vuex'
 export default {
 	data() {
+		const validateUsername = (rule, value, callback) => {
+			if (!value) {
+				callback(new Error('请输入用户名！'))
+			} else {
+				callback()
+			}
+		};
+		const validatePsd = (rule, value, callback) => {
+			if (!value) {
+				callback(new Error('请输入密码！'))
+			} else {
+				callback()
+			}
+		};
 		return {
 			loading: false,
-			form: {}
-		}
+			form: {},
+			rules: {
+				username: [{ validator: validateUsername, trigger: 'blur' }],
+				password: [{ validator: validatePsd, trigger: 'blur' }]
+			}
+		};
 	},
-	computed:{
-	
-	},
-	methods:{
+	computed: {},
+	methods: {
 		...mapActions({ loginByPwd: 'user/loginByPwd' }),
 		handleLogin() {
-			this.loading = true
-			this.loginByPwd(this.form).then(() => {
-				this.loading = false
-				this.$router.push('/home')
-			}).catch(err => {
-				this.loading = false
-				throw new Error(err)
+			this.$refs.form.validate(valid => {
+				if (valid) {
+					this.loading = true
+					this.loginByPwd(this.form).then(() => {
+						this.loading = false
+						this.$router.push('/home')
+					}).catch(err => {
+						this.loading = false
+						throw new Error(err)
+					});
+				} else {
+					console.log('error submit!!')
+					return false
+				}
 			})
 		}
 	}
@@ -43,5 +69,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import '@/styles/mixin.scss';
+.login {
+	background: url(../../assets/login_bg.jpg) no-repeat;
+	background-size: cover;
+	height: 100%;
+	.login-form-wrapper {
+		width: 360px;
+		@include pCenter;
+		.login-title {
+			color: #1ab392;
+			font-weight: normal;
+			font-size: 24px;
+			text-align: center;
+			margin-bottom: 30px;
+		}
+		.login-form {
+			.el-form-item {
+				margin-bottom: 30px;
+				.el-input {
+					height: 42px;
+					background-color: transparent;
+					/deep/ .el-input__inner {
+						background-color: rgba(255, 255, 255, 0.2);
+						border: none;
+						border-radius: 0;
+						color: #fff;
+					}
+				}
+			}
+		}
+		.login-btn {
+			background-color: #1ab392;
+			border-color: #1ab392;
+			width: 100%;
+			border-radius: 0;
+		}
+	}
+}
 </style>
