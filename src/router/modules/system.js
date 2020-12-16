453221002a4f6dea7d94085467c72eee542d6ab6
wwb <1007575954@qq.com>
2020-12-16 15:10:22 +0800

    wwb

--- src/router/modules/system.js
+++ src/router/modules/system.js
@@ -5,23 +5,23 @@ const system = {
 	name: 'System',
 	component: Layout,
 	children: [{
-			path: 'user',
-			name: 'SystemUser',
-			component: () => import('@/views/System/User'),
-			meta: {
-				title: '用户管理',
-				menuIcon: 'el-icon-setting'
-			}
-		},
-		{
-			path: 'role',
-			name: 'SystemRole',
-			component: () => import('@/views/System/Role'),
-			meta: {
-				title: '角色管理',
-				menuIcon: 'el-icon-s-custom'
-			}
+		path: 'user',
+		name: 'SystemUser',
+		component: () => import('@/views/System/User'),
+		meta: {
+			title: '用户管理',
+			menuIcon: 'el-icon-setting'
 		}
+	},
+	{
+		path: 'role',
+		name: 'SystemRole',
+		component: () => import('@/views/System/Role'),
+		meta: {
+			title: '角色管理',
+			menuIcon: 'el-icon-s-custom'
+		}
+	}
 	]
 }
 
--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -1,37 +1,37 @@
 import Layout from '@/views/Layout'
 
 const user = {
-	path: '/user',
-	name: 'User',
-	component: Layout,
-	children: [{
-			path: 'info',
-			name: 'UserInfo',
-			component: () => import('@/views/User/Info'),
-			meta: {
-				title: '用户信息',
-				menuIcon: 'el-icon-setting'
-			}
-		},
-		{
-			path: 'resetPsd',
-			name: 'UserResetPsd',
-			component: () => import('@/views/User/ResetPsd'),
-			meta: {
-				title: '修改密码',
-				menuIcon: 'el-icon-setting'
-			}
-		},
-		{
-			path: 'permission',
-			name: 'UserPermission',
-			component: () => import('@/views/User/Permission'),
-			meta: {
-				title: '页面权限',
-				menuIcon: 'el-icon-s-tools'
-			}
-		}
-	]
+  path: '/user',
+  name: 'User',
+  component: Layout,
+  children: [{
+    path: 'info',
+    name: 'UserInfo',
+    component: () => import('@/views/User/Info'),
+    meta: {
+      title: '基础信息',
+      menuIcon: 'el-icon-setting'
+    }
+  },
+  {
+    path: 'resetPsd',
+    name: 'UserResetPsd',
+    component: () => import('@/views/User/ResetPsd'),
+    meta: {
+      title: '修改密码',
+      menuIcon: 'el-icon-setting'
+    }
+  },
+  {
+    path: 'permission',
+    name: 'UserPermission',
+    component: () => import('@/views/User/Permission'),
+    meta: {
+      title: '页面权限',
+      menuIcon: 'el-icon-s-tools'
+    }
+  }
+  ]
 }
 
 export default user
